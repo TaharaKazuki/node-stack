082b2b0ffdd81aec2be304210de5d971f807b3a0
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-22 14:18:46 +0900

    setting fastify

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
-'use strict';
-console.info('hello world');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const fastify = fastify_1.default({ logger: true });
+fastify.get('/', async () => {
+    return {
+        hello: 'world',
+    };
+});
+const port = process.env.PORT ?? 8080;
+const start = async () => {
+    try {
+        await fastify.listen(port, '0.0.0.0');
+    }
+    catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+start();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.info('hello world')
+import Fastify from 'fastify'
+
+const fastify = Fastify({ logger: true })
+
+fastify.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+const port = process.env.PORT ?? 8080
+const start = async () => {
+  try {
+    await fastify.listen(port, '0.0.0.0')
+  } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+  }
+}
+
+start()
